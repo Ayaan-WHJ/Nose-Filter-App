de52a59a3f042a9e043bf1042b8afebc67e866e2
Ayaan-WHJ <78539077+Ayaan-WHJ@users.noreply.github.com>
2021-05-12 16:41:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+noseX = 0;
+noseY = 0;
 function preload()
 {
-
+clown_nose = loadImage('https://i.postimg.cc/KYTj17WP/clownnose.png');
 }
 
 function setup()
@@ -16,6 +18,8 @@ function setup()
 
 function draw(){ 
     image(video,0,0,300,300);
+   
+    image(clown_nose,noseX,noseY,30,30);
 }
 
 function take_snapshot()
@@ -33,7 +37,9 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
-        console.log("nose x =" + results[0].pose.nose.x);
-        console.log("nose y =" + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x-15;
+        noseY = results[0].pose.nose.y-15;
+        console.log("nose x =" + noseX);
+        console.log("nose y =" + noseY);
     }
 }
